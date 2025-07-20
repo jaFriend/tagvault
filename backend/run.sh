57c57b389fabec766829bd401ee74d29979fed7d
Joseph Friend <josephangelfriend1@gmail.com>
2025-07-20 03:13:34 -0700

    Adding Backend CI/CD and some fixes on JWT

--- backend/run.sh
+++
@@ -1,5 +0,0 @@
-docker run \
-  --env-file .env \
-  -v ./config/clerk_key.pem:/app/config/clerk_key.pem \
-  -p 5050:5050 \
-  tagvault-backend:latest
--- backend/validators/jwtauth.js
+++ backend/validators/jwtauth.js
@@ -4,9 +4,7 @@ import path from 'path';
 import Cookies from 'cookies';
 import jwt from 'jsonwebtoken';
 
-
 const clerk_pem_file = process.env.CLERK_PEM_FILE;
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const pubKeyPath = path.join(__dirname, `../config/${clerk_pem_file}`);
